a03823a7160ab8059ed96be7560be82c2a0c11af
cheetosysst <thect@protonmail.com>
2020-09-25 22:42:19 +0800

    Init

---
+++ install.sh
@@ -0,0 +1,10 @@
+pacstrap /mnt \
+	base linux linux-firmware \
+	networkmanager \
+	grub \
+	mesa \
+	bluez pulseaudio-alsa \
+	sddm xfce4 awesome\
+	konsole firefox spectacle thunar \
+	ttf-freefont ttf-roboto noto-fonts noto-fonts-emoji noto-fonts-cjk adobe-source-code-pro-fonts \
+	sudo vim git base-devel
